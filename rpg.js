f894003479b9141cd582cd2a34a6061066bdb8d7
rodrigoliva <rodrigolivavelho@gmail.com>
2020-05-03 04:13:57 -0300

    Configurada na lógica

--- rpg.js
+++ rpg.js
@@ -1,24 +1,33 @@
-function Person(first, last, age, eye) {
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
-  this.eyeColor = eye;
+const pontosTotais = 12
+
+
+const dado = () => {
+    return Math.round((Math.random() * 5) + 1)
 }
 
-var myFather = new Person("John", "Doe", 50, "blue"); 
+function personagem(frc, hab, res, arm, pdf, PV, PM, msg, money, historia) {
+    this.frc = frc;
+    this.hab = hab;
+    this.res = res;
+    this.arm = arm;
+    this.pdF = pdf;
+    this.PV = res * 5;
+    this.PM = res * 5;
+    this.money = dado() * 100;
+    this.historia = historia
+    this.msg = `Esse personagem tem ${frc} de Força, ${hab} de Habilidade, ${res} de Resistência, ${arm} de Armadura ${pdf} de Poder de Fogo, ${res * 5} Pontos de vida e ${res * 5} Pontos de Magia `;
+}
 
-Força
-Habilidade 
-Resistência 
-Armadura  
-Poder de Fogo
+
+
+pericias = ["Animais", "Arte", "Ciência", "Crime", "Esporte", "Idiomas", "Investigação", "Manipulação", "Máquinas", "Medicina", "Sobrevivência"] // 2 pontos
+
+
+var teste = new personagem(1, 1, 5, 1, 1, 1, 1,)
+
+console.log(teste)
 
 
 
-Animais, Arte, Ciência, Crime, Esporte, Idiomas, Investigação, Manipulação,
-Máquinas, Medicina e Sobrevivência.
 
 
-1dx100 Moedas
-
-function dado
