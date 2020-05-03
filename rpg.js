01c11202e7a96b178a2e502ce1f0abf07e8109ff
rodrigoliva <rodrigolivavelho@gmail.com>
2020-05-03 02:02:03 -0300

    First Commit

---
+++ rpg.js
@@ -0,0 +1,24 @@
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+var myFather = new Person("John", "Doe", 50, "blue"); 
+
+Força
+Habilidade 
+Resistência 
+Armadura  
+Poder de Fogo
+
+
+
+Animais, Arte, Ciência, Crime, Esporte, Idiomas, Investigação, Manipulação,
+Máquinas, Medicina e Sobrevivência.
+
+
+1dx100 Moedas
+
+function dado
